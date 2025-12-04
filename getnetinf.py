5a0902e685dd988b9eda0cc787b245db6c3e521d
Maestrum0001 <188807426+Maestrum0001@users.noreply.github.com>
2025-12-04 07:50:39 -0600

    Update getnetinf.py
    Removed the commented code that was re-iterated into the main.py file.

--- getnetinf.py
+++ getnetinf.py
@@ -4,21 +4,3 @@ import json
 def get_mac():
 	mac_address = get_mac_address()
 	return mac_address
-'''
-def write_latest(mac):
-	data = {
-		"mac": mac,
-	}
-
-	with open("readings.json", "w") as f:
-		json.dump(data, f, indent=4)
-
-def main():
-	mac = get_mac()
-	print(f"MAC Address (using getmac): {get_mac_address()}")
-	write_latest(mac)
-
-if __name__ == "__main__":
-	main()
-
-'''
