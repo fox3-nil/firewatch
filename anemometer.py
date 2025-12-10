092cd3fbae6a0578f6738d42d7f36c24cbb057c9
Boyle, Wesley <wesleyboyle@my.unt.edu>
2025-12-10 15:25:07 -0600

    Added test function for wind vane

--- anemometer.py
+++ anemometer.py
@@ -1,6 +1,13 @@
 import RPi.GPIO as GPIO
+import board
 import time
+from adafruit_ads1x15 import ADS1015, AnalogIn, ads1x15
 import math
+
+i2c = board.I2C()
+ads_anemometer = ADS1015(i2c,gain = 2/3, address=0x4B) #alternate address
+
+wind_vane = AnalogIn(ads_anemometer, ads1x15.A0)
 
 Anemometer_PIN = 17 #GPIO Pin on raspi
 
@@ -17,6 +24,47 @@ K = 0.667 #conversion constant from mph to m/s
 #Global variable to store pulse count to use as interrupt
 pulse_count = 0
 last_event_time = time.time() #Used for debouncing
+
+def wind_vane_direction():
+    V_IN = 5.0
+    R2 = 10000 #static transistor
+    wind_vane_angle = {
+		#Resistance : Angle
+		33000.0 : 0,
+		6570.0 : 22.5,
+		8200.0 : 45,
+		891.0 : 67.5,
+		1000.0 : 90,
+		688.0 : 112.5,
+		2200.0 : 135,
+		1410.0 : 157.5,
+		3900.0 : 180,
+		3140.0 : 202.5,
+		16000.0 : 225,
+		14120.0 : 247.5,
+		120000.0 : 270,
+		42120.0 : 292.5,
+		64900.0 : 315,
+		21880.0 : 337.5,
+	}
+    
+    measured_R = (wind_vane.voltage * R2) / (V_IN - wind_vane.voltage) #
+    
+    key_r = closest_resistance(measured_R, wind_vane_angle)
+    
+    return wind_vane_angle[key_r]
+
+def closest_resistance(R1, direction_key):
+    closest_resistance = None
+    min_difference = float('inf')
+     
+    for key_r in direction_key.keys():
+         current_difference = abs(R1 - key_r)
+         
+         if current_difference < min_difference:
+             min_difference = current_difference
+             closest_resistance = key_r
+    return closest_resistance
 
 def spin_callback(channel): #interrupt to count pulse_count
 	global pulse_count, last_event_time
@@ -60,8 +108,10 @@ try:
         
         # Perform calculation
 		speed_ms, speed_mph, frequency = calculate_wind_speed()
-        
+  
+		current_angle = wind_vane_direction()
         # Print results
+		print(f"--Current Angle: {current_angle} degrees")
         # print(f"--- Measurement ({time.strftime('%H:%M:%S')}) ---")
 		print(f"Pulse Frequency: {frequency:.2f} Hz")
 		print(f"Wind Speed: **{speed_ms:.2f} m/s** ({speed_mph:.2f} MPH)")
