9f9902118c9c1d38415530b0b3fa8546452144c2
Maestrum0001 <zaydenowens60206@gmail.com>
2025-12-09 22:34:19 -0600

    Added serial debugging program and systemd file initialization in firewatch_libs

--- firewatch_libs.sh
+++ firewatch_libs.sh
@@ -13,7 +13,7 @@ fi
 
 VENV_DIR="FireWatch_VENV"
 
-echo "[1/6] Checking for virtual environment directory"
+echo "[1/7] Checking for virtual environment directory"
 if [[ ! -d "$VENV_DIR" ]]; then
     echo "No venv found. Creating one..."
     python3 -m venv "$VENV_DIR"
@@ -21,22 +21,22 @@ else
     echo "Existing venv detected."
 fi
 
-echo "[2/6] Activating virtual environment..."
+echo "[2/7] Activating virtual environment..."
 #shellcheck source=/dev/null
 source "$VENV_DIR/bin/activate"
 
 echo "Python executable now: $(which python3)"
 
-echo "[3/6] Updating apt..."
+echo "[3/7] Updating apt..."
 sudo apt update
 
-echo "[4/6] Installing system packages..."
+echo "[4/7] Installing system packages..."
 sudo apt install -y python3-pip python3-smbus i2c-tools python3-pil libcamera-tools python3-libcamera
 
-echo "[5/6] Upgrading pip..."
+echo "[5/7] Upgrading pip..."
 pip3 install --upgrade pip
 
-echo "[6/6] Installing Python libraries..."
+echo "[6/7] Installing Python libraries..."
 pip3 install \
     adafruit-blinka \
     adafruit-circuitpython-ahtx0 \
@@ -44,6 +44,46 @@ pip3 install \
     smbus2 \
     getmac \
 
+echo "[7/7] Creating systemd service..."
+
+SERVICE_FILE="/etc/systemd/system/firewatch-debug.service"
+
+sudo bash -c "cat > $SERVICE_FILE" << 'EOF'
+
+[Unit]
+Description=FireWatch Sensor Debugging Service
+After=network.target
+
+[Service]
+WorkingDirectory=/home/firewatch/firewatch
+Environment="PATH=/home/firewatch/firewatch/FireWatch_VENV/bin"
+ExecStart=/home/firewatch/firewatch/FireWatch_VENV/bin/python /home/firewatch/firewatch/serialdebug.py
+
+# Auto-restart on failure
+Restart=always
+RestartSec=3
+
+User=firewatch
+Group=firewatch
+
+# Systemd logs instead of TTY
+StandardOutput=journal
+StandardError=journal
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Reloading systemd..."
+sudo systemctl daemon-reload
+
+echo "Enabling service..."
+sudo systemctl enable firewatch-debug.service
+
+echo "Starting service..."
+sudo systemctl start firewatch-debug.service
+
+echo "Serial Debug service installed and running!"
 
 echo "=== Installation Complete! ==="
 echo "All required libraries are installed."
---
+++ serialdebug.py
@@ -0,0 +1,94 @@
+import os
+import sys
+import time
+import random
+#import internaltnh
+#import externaltnh
+#import memssuite_probe
+import serial
+#import co_probe
+
+
+def clear_screen():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+
+ser = serial.Serial('/dev/serial0', 115200, timeout=1)
+
+def serial_print(msg):
+	print(msg)
+	ser.write((msg + "\r\n").encode())
+
+def get_sensor_values():
+
+	#THIS IS WHERE YOU READ SENSOR VALUES AND VERIFY THEIR OUTPUT
+#	aht20_t = internaltnh.get_temperature()
+#	aht20_h = internaltnh.get_humidity()
+#	cht832x_t, cht832x_h = externaltnh.sensor_oneshot()
+#	fermion_co, fermion_voc, fermion_smoke, fermion_methane = memssuite_probe.memssuite()
+
+	aht20_t = random.uniform(20.00, 55.00)
+	aht20_h = random.uniform(45.00, 47.00)
+	cht832x_t = random.uniform(10, 15)
+	cht832x_h = random.uniform(55, 65)
+	fermion_voc = random.uniform(0.4, 5)
+	fermion_methane = random.uniform(0.4, 5)
+	fermion_smoke = random.uniform(0.4, 5)
+	fermion_co = random.uniform(0.4, 5)
+
+	batterylvl = random.randint(1, 100)
+	return aht20_t, aht20_h, cht832x_t, cht832x_h, fermion_voc, fermion_methane, fermion_smoke, fermion_co, batterylvl
+
+def main():
+
+	try:
+		while True:
+			itemp, ihumd, etemp, ehumd, vocs, methane, smoke, carbmono, battery = get_sensor_values()
+
+			clear_screen()
+			serial_print("\n=== SENSOR DEBUGGING TEST ===")
+			serial_print(f"AHT20 Temperature:     {itemp:.2f} °C")
+			serial_print(f"AHT20 Humidity:        {ihumd:.2f} %")
+			serial_print(f"CHT832X Temperature:   {etemp:.2f} °C")
+			serial_print(f"CHT832X Humidity:      {ehumd:.2f} %")
+			serial_print(f"Fermion VOC Vout:      {vocs:.2f} V")
+			serial_print(f"Fermion Methane Vout:  {methane:.2f} V")
+			serial_print(f"Fermion Smoke Vout:    {smoke:.2f} V")
+			serial_print(f"Fermion CO Vout:       {carbmono:.2f} V")
+			serial_print(f"Battery Level:         {battery} %\n")
+			serial_print("      === RESULTS ===\n")
+
+			if itemp <= 51.66:
+				serial_print("AHT20 Temperature ==============	[PASSED]")
+			else:
+				serial_print("AHT20 Temperature ==============	[WARNING]")
+			if etemp <= 43.33:
+				serial_print("CHT832X Temperature ============	[PASSED]")
+			else:
+				serial_print("CHT832X Temperature ============	[WARNING]")
+			if 0.00 <= vocs <= 1.40:
+				serial_print("VOC V OUTPUT ===================	[PASSED]")
+			else:
+				serial_print("VOC V OUTPUT ===================	[WARNING]")
+			if 0.00 <= methane <= 2.00:
+				serial_print("METHANE V OUTPUT ===============	[PASSED]")
+			else:
+				serial_print("METHANE V OUTPUT ===============	[WARNING]")
+			if 0.00 <= smoke <= 0.5:
+				serial_print("SMOKE V OUTPUT =================	[PASSED]")
+			else:
+				serial_print("SMOKE V OUTPUT =================	[WARNING]")
+			if 0.00 <= carbmono <= 0.90:
+				serial_print("CARBON MONOXIDE V OUTPUT =======	[PASSED]")
+			else:
+				serial_print("CARBON MONOXIDE V OUTPUT =======	[WARNING]")
+
+			print("\n")
+			time.sleep(1)
+
+	except KeyboardInterrupt:
+		serial_print("\nDEBUG STOPPED\n")
+		sys.exit(0)
+
+if __name__ == "__main__":
+	main()
